e98a91c7a4755249d649f7d2132026e5f9bf51cd
root <fabian.spiri@swisscom.com>
2026-05-19 09:32:10 +0200

    feat: small error (eslint) replaced in main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
-import helmet from 'helmet';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+import { ValidationPipe } from "@nestjs/common";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,13 +19,13 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: (process.env.CORS_ORIGIN || 'http://localhost:5173').split(','),
+    origin: (process.env.CORS_ORIGIN || "http://localhost:5173").split(","),
     credentials: true,
   });
 
   const config = app.get(ConfigService);
-  const port = config.get<number>('PORT', 3000);
+  const port = config.get<number>("PORT", 3000);
 
   await app.listen(port);
 }
-bootstrap();
+void bootstrap();
