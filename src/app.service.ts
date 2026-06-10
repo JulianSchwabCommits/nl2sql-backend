cd63697ceaaa3a84b025994f7648e200aa7bd220
julian schwab <jla.schwab@gmail.com>
2026-06-10 14:23:30 +0200

    test: trigger consolidated CI/CD pipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   ItsRunning(): string {
-    return "It Works! Testing workflow_run pipeline";
+    return "It Works! Testing consolidated CI/CD pipeline";
   }
 }
